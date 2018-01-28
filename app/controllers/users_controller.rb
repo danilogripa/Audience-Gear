5d5eeffb9a8400967b7e2a20875db10fe4e5a676
Glauco Foeppel <glauco.tf@gmail.com>
2018-01-27 23:22:16 -0200

    Adiciona area de login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :authenticate_admin_user!
+
 	def index
 		@users = User.paginate(:page => params[:page], :per_page => 10)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
   resources :users
   resources :sites
+
+  root to: "users#index"
+
 end
