49aee47f09ae1a9ebd4f753043f443d77f69ca74
Glauco Foeppel <glauco.tf@gmail.com>
2018-01-31 03:07:29 -0200

    FUNCIONOU CARALHOOOOOO

--- app/controllers/retargeting_controller.rb
+++ app/controllers/retargeting_controller.rb
@@ -1,11 +1,13 @@
 class RetargetingController < ActionController::Base
 
+  layout false
+
   def index
     @campaign = Campaign.find params[:campaign]
     @affiliates = []
     if @campaign.status? == true
       @affiliates = @campaign.affiliates.where(active: true)
     end
+    self.content_type = "application/javascript;"
   end
-
 end
