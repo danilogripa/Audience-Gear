97586b543d1d76b8be7dca4f91b21084087a6f3b
Glauco Foeppel <glauco.tf@gmail.com>
2018-02-16 13:22:21 -0200

    Add tags da campanha para o afiliado

--- app/controllers/retargeting_controller.rb
+++ app/controllers/retargeting_controller.rb
@@ -4,12 +4,15 @@ class RetargetingController < ActionController::Base
 
   def index
     @campaign = Campaign.find params[:campaign]
-    ahoy.track_visit if ahoy.new_visit?
-    ahoy.track @campaign.name, title: "Hot, Flat, and Crowded"
     @affiliates = []
     if @campaign.status? == true
-      @affiliates = @campaign.affiliates.where(active: true)
+      @campaign.affiliates.where(active: true).each do |affiliate|
+        affiliate.partner = affiliate.partner.gsub("{CAMPAIGN_ID}", @campaign.id.to_s)
+        affiliate.partner = affiliate.partner.gsub("{CAMPAIGN_NAME}", @campaign.name.to_s)
+        @affiliates << affiliate
+      end
     end
     self.content_type = "application/javascript;"
   end
+
 end
